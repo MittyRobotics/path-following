5a38ef7744d9f73a0991de36d0942b7024a5bd81
AndyLi23 <andyliqy@gmail.com>
2021-11-27 20:09:49 -0800

    add gauss coefficients for n=2 to n=20

--- path-following/src/splines/Test.java
+++ path-following/src/splines/Test.java
@@ -8,18 +8,22 @@ public class Test {
     public static void main(String[] args) {
         QuinticHermiteSpline spline = new QuinticHermiteSpline(
                 new Pose2D(0, 0, 0),
-                new Pose2D(0, 50, 0)
+                new Pose2D(100, 50, -1)
         );
 
         DecimalFormat df = new DecimalFormat("#");
-        df.setMaximumFractionDigits(5);
+        df.setMaximumFractionDigits(10);
 
-        for(double i = 0; i <= 1.0; i+=0.01) {
+        /*for(double i = 0; i <= 1.0; i+=0.01) {
             System.out.print("(" + df.format(spline.getPoint(i).getX()) + ", " + df.format(spline.getPoint(i).getY()) + "),");
         }
-        System.out.println();
+        System.out.println();*/
 
-        System.out.println(spline.getRawLength(0., 1., 10000));
-        System.out.println(spline.getGaussianQuadratureLength(0., 1.));
+        /*double actualLength = spline.getRawLength(0., 1., 100000);
+        for(int i = 2; i <= 20; i++) {
+            System.out.println("n=" + i + " | val=" + spline.getGaussianQuadratureLength(0., 1., i)
+            + " | error=" + (actualLength - spline.getGaussianQuadratureLength(0., 1., i)) + " | percenterror = " +
+                    df.format((actualLength - spline.getGaussianQuadratureLength(0., 1., i))/actualLength));
+        }*/
     }
 }
